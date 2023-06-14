899ed27fb86e12a6ca1171a40f5d712a963648f1
VarshaShaheen <varshashaheen2003@gmail.com>
2023-06-14 13:25:14 +0530

    data added

---
+++ js/load.js
@@ -0,0 +1,24 @@
+const mp = document.getElementById('mp');
+
+function getRow(...columns)
+{
+    const row = document.createElement('tr');
+    for(const column of columns)
+    {
+        const td = document.createElement('td');
+        td.innerText = column || "-";
+        row.appendChild(td);
+    }
+    return row;
+}
+
+Papa.parse("/data/mp.csv", {
+    download: true,
+    complete: function(results) {
+        for(const row of results.data)
+        {
+            mp.appendChild(getRow(...row));
+        }
+    }
+});
+
